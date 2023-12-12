1a3ad0f0f4dad881031ed5e665119fba2cd2b0b5
RubbishCode <rubbishcode@gmail.com>
2023-12-12 17:41:30 +0300

    update

--- src/Message/PayoutResponse.php
+++ src/Message/PayoutResponse.php
@@ -7,4 +7,23 @@ use Omnipay\Common\Message\AbstractResponse;
 class PayoutResponse extends AbstractResponse
 {
 
+    public function isSuccessful()
+    {
+        if (isset($this->data['status'])) {
+            if ($this->data['status'] == 'ok') {
+                return true;
+            }
+        }
+    }
+
+    public function getTransactionReference()
+    {
+        return isset($this->data['tx']['tx']) ? $this->data['tx']['tx'] : null;
+    }
+
+    public function getMessage()
+    {
+        return isset($this->data['message']) ? json_encode($this->data['message']) : null;
+    }
+
 }
