0aa9b114735ca2b5cd87877ef502a95f183b9bf2
RubbishCode <rubbishcode@gmail.com>
2023-12-12 21:38:01 +0300

    add

--- src/Message/PayoutRequest.php
+++ src/Message/PayoutRequest.php
@@ -3,6 +3,7 @@
 namespace Omnipay\InterKassa\Message;
 
 use Omnipay\Common\Message\AbstractRequest;
+use Omnipay\PayPlanet\Message\MapperCodeCurrency;
 
 
 class PayoutRequest extends AbstractRequest
@@ -128,30 +129,6 @@ class PayoutRequest extends AbstractRequest
         return substr($result, 0, -1);
     }
 
-    private function prepareMultipartData($data, $prefix = '')
-    {
-        $multipartData = [];
-        foreach ($data as $key => $value) {
-            $fullKey = $prefix . $key;
-            if (is_array($value)) {
-                // Обработка вложенных массивов
-                foreach ($value as $subKey => $subValue) {
-                    $multipartData[] = [
-                        'name'     => "{$fullKey}[{$subKey}]",
-                        'contents' => $subValue
-                    ];
-                }
-            } else {
-                // Обработка простых значений
-                $multipartData[] = [
-                    'name'     => $fullKey,
-                    'contents' => $value
-                ];
-            }
-        }
-        return $multipartData;
-    }
-
 
     public function sendData($data)
     {
@@ -173,46 +150,28 @@ class PayoutRequest extends AbstractRequest
                 'phone' => "00000000000"
             ]
         ];
+        $userId = $this->getUserApi();
+        $apiKey = $this->getKeyApi();
 
-        // Подготовка данных запроса и заголовков
-        $userId = $this->getUserApi(); // Убедитесь, что это строка
-        $apiKey = $this->getKeyApi(); // Убедитесь, что это строка
         $authHeaderValue = 'Basic ' . base64_encode($userId . ':' . $apiKey);
 
         $checkoutKey = $this->getSecretKey();
         $sortedDataByKeys = $this->sortByKeyRecursive($data);
         $sortedDataByKeys[] = $checkoutKey;
         $signString = $this->implodeRecursive(':', $sortedDataByKeys);
-//        $data['ik_sign'] = base64_encode(hash('sha256', $signString, true));
+        $data['ik_sign'] = base64_encode(hash('sha256', $signString, true));
 
-        $multipartData = $this->prepareMultipartData($data);
+        $postData = http_build_query($data);
 
-        // Создаем HTTP-клиент Guzzle
-        $client = new \GuzzleHttp\Client();
-
-        // Выполняем запрос
-        try {
-            $response = $client->request('POST', 'https://api.interkassa.com/v1/withdraw', [
-                'headers' => [
-                    'Authorization' => $authHeaderValue,
-                    'Ik-Api-Account-Id' => $userId,
-                ],
-                'multipart' => $multipartData
-            ]);
-        } catch (\Exception $e) {
-            // Обработка ошибок запроса
-            // Можно логировать или пробрасывать исключение дальше
-            error_log($e->getMessage());
-            throw $e;
-        }
-
-        // Обработка ответа для Omnipay
-        return $this->response = $this->createResponse($response->getBody()->getContents());
+        $headers = [
+            'Content-Type' => 'application/x-www-form-urlencoded',
+            'Authorization' => $authHeaderValue,
+            'Ik-Api-Account-Id' => $userId
+        ];
+        // Отправляем запрос
+        $httpResponse = $this->httpClient->request('POST', 'https://api.interkassa.com/v1/withdraw', $headers, $postData);
+        return $this->createResponse($httpResponse->getBody()->getContents());
     }
-
-
-
-
 
 
     protected function createResponse($data)
